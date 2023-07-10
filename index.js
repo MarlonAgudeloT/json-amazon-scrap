1dadca5cf52a73df34d80d508a11e8c5057f1edf
MarlonAgudelo <mgagudelot@gmail.com>
2023-07-10 10:27:39 -0500

    Loading files to git

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//const apiKey = 'b7673e49b853f6bc551cba2df1d6da63';
+//const generateScraperUrl(apiKey) = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`; 
+
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to Amazon Scraper API')
+});
+
+//GET Product details
+app.get('/products/:productId',async(req,res)=>{
+    const{ productId } = req.params;
+    const{ api_key } = req.query;
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/dp/${productId}`);
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+})
+
+//GET Product Reviews
+app.get('/products/:productId/reviews',async(req,res)=>{
+    const{ productId } = req.params;
+    const{ api_key } = req.query;
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+})
+
+//GET Product Offers
+app.get('/products/:productId/offers',async(req,res)=>{
+    const{ productId } = req.params;
+    const{ api_key } = req.query;
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+})
+
+//GET Search Results
+app.get('/search/:searchQuery',async(req,res)=>{
+    const{ searchQuery } = req.params;
+    try{
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+})
+
+
+app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
